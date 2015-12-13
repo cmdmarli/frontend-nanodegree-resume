4bf9b15328cc1d094b9aa83b6e3246c268568087
cmdmarli <marliduplessis@Marlis-MacBook-Pro.local>
2015-12-13 09:52:50 +0200

    Styling

--- js/helper.js
+++ js/helper.js
@@ -15,36 +15,38 @@ replace the %data% placeholder text you see in them.
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
 var HTMLheaderRole = '<span>%data%</span><hr/>';
 
+var HTMLskillsStart = '<div class="skills-entry"></div>';
 var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
-var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
-var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
-var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
-var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
+var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile:</span><span class="white-text">%data%</span></li>';
+var HTMLemail = '<li class="flex-item"><span class="orange-text">email:</span><span class="white-text">%data%</span></li>';
+var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter:</span><span class="white-text">%data%</span></li>';
+var HTMLgithub = '<li class="flex-item"><span class="orange-text">github:</span><span class="white-text">%data%</span></li>';
 var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
-var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
+var HTMLlocation = '<li class="flex-item"><span class="orange-text">location:</span><span class="white-text">%data%</span></li>';
 
 var HTMLbioPic = '<img src="%data%" class="biopic">';
-var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
+var HTMLwelcomeMsg = '<p class="welcome-message">%data%</p><hr/>';
 
-var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';
+var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills"</ul>';
 var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
 
 var HTMLworkStart = '<div class="work-entry"></div>';
-var HTMLworkEmployer = '<a href="https://www.google.co.za">%data%';
-var HTMLworkTitle = ' - %data%</a>';
+var HTMLworkEmployer = '<h3>%data%</h3>';
+var HTMLworkTitle = '<h4>%data%</h4>';
 var HTMLworkDates = '<div class="date-text">%data%</div>';
 var HTMLworkLocation = '<div class="location-text">%data%</div>';
-var HTMLworkDescription = '<p><br>%data%</p>';
+var HTMLworkDescription = '<p>%data%</p>';
 
 var HTMLprojectStart = '<div class="project-entry"></div>';
-var HTMLprojectTitle = '<a href="#">%data%</a>';
+var HTMLprojectTitle = '<h3>%data%</h3>';
+var HTMLprojectRole = '<h4>%data%</h4>';
 var HTMLprojectDates = '<div class="date-text">%data%</div>';
-var HTMLprojectDescription = '<p><br>%data%</p>';
+var HTMLprojectDescription = '<p>%data%</p>';
 var HTMLprojectImage = '<img src="%data%">';
 
 var HTMLschoolStart = '<div class="education-entry"></div>';
-var HTMLschoolName = '<a href="#">%data%';
-var HTMLschoolDegree = ' -- %data%</a>';
+var HTMLschoolName = '<h3>%data%</h3>';
+var HTMLschoolDegree = '<h4>%data%</h4>';
 var HTMLschoolDates = '<div class="date-text">%data%</div>';
 var HTMLschoolLocation = '<div class="location-text">%data%</div>';
 var HTMLschoolMajor = '<em><br>Major: %data%</em>';
@@ -55,8 +57,9 @@ var HTMLonlineSchool = ' - %data%</a>';
 var HTMLonlineDates = '<div class="date-text">%data%</div>';
 var HTMLonlineURL = '<br><a href="#">%data%</a>';
 
-var internationalizeButton = '<button>Internationalize</button>';
 var googleMap = '<div id="map"></div>';
+
+var internationalizeButton = '<button>Internationalize</button>';
 
 
 /*
@@ -85,7 +88,10 @@ function logClicks(x,y) {
 }
 
 $(document).click(function(loc) {
-  // your code goes here!
+  var x = loc.pageX;
+  var y = loc.pageY;
+
+  logClicks(x,y);
 });
 
 
@@ -235,11 +241,11 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
+window.addEventListener('resize', function(e) {
   //Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+  map.fitBounds(mapBounds);
+});
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,19 @@
+
+
+function inName(name)
+	{
+	name = name.trim().split(" ");
+	console.log(name);
+	name[1] = name[1].toUpperCase();
+	name[0] = name[0].slice(0,1).toUpperCase() +
+		name[0].slice(1).toLowerCase();
+
+	return name [0] + name [1];
+	}
+
+$("#main").append(internationalizeButton);
+
+
 /*
 Header
 */
@@ -5,33 +21,39 @@ Header
 var bio = {
 	"name" : "Marli du Plessis",
 	"role" : "Front-end Developer",
+	"welcomeMessage" : "Working in the fast paced industry of online banking and ample experience in digital advertising - I  developed a keen sense for digital & mobile advertising, social media stratagies and user experience and interface design within the South African as well as African markets.",
 	"contacts" : {
 		"mobileInfo" : "(+27) 82 839 3375",
 		"githubInfo" : "cmdmarli",
 		"emailInfo" : "cmd.marli@gmail.com",
-		"locationInfo" : "South Africa, Johannesburg",
-		"twitter" : "@cmdmarli",
-		"welcomeMessage" : "Working in the fast paced industry of digital advertising and banking I have a keen sense for digital & mobile advertising; social media; user experience and interface design within the South African as well as African markets."
+		"location" : "Bramley, Johannesburg, South Africa",
+		"twitter" : "@cmdmarli"
 	},
 	"skills" : [
-		"UI design", "UX design", "Front-end development", "Animation"
+		"User Interface design", "User Experience design", "Front-end Development", "Creative Direction", "Graphic design"
 	],
 	"pictureURL" : "images/marli.jpg"
 };
 
+		$("#header").prepend(HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage));
 		$("#header").prepend(HTMLheaderRole.replace("%data%", bio.role));
 		$("#header").prepend(HTMLheaderName.replace("%data%", bio.name));
 		$("#topContacts, #footerContacts").append(HTMLmobile.replace("%data%", bio.contacts.mobileInfo));
 		$("#topContacts, #footerContacts").append(HTMLemail.replace("%data%", bio.contacts.emailInfo));
 		$("#topContacts, #footerContacts").append(HTMLgithub.replace("%data%", bio.contacts.githubInfo));
-		$("#topContacts, #footerContacts").append(HTMLlocation.replace("%data%", bio.contacts.locationInfo));
+		$("#topContacts, #footerContacts").append(HTMLlocation.replace("%data%", bio.contacts.location));
 		//$("#topContacts").append(HTMLtwitter.replace("%data%", bio.contacts.twitter));
 		$("#header").append(HTMLbioPic.replace("%data%", bio.pictureURL));
+
+if(bio.skills.length > 0) {
+
 		$("#header").append(HTMLskillsStart);
 		$("#header").append(HTMLskills.replace("%data%", bio.skills[0]));
 		$("#header").append(HTMLskills.replace("%data%", bio.skills[1]));
 		$("#header").append(HTMLskills.replace("%data%", bio.skills[2]));
 		$("#header").append(HTMLskills.replace("%data%", bio.skills[3]));
+		$("#header").append(HTMLskills.replace("%data%", bio.skills[4]));
+		}
 
 /*
 Work Experience
@@ -45,34 +67,34 @@ var work =
 			"position" : "Mid-Weight Creative",
 			"employer" : "Gloo@Ogilvy",
 			"workdates" : "3 Jan 2012 - 31 Junie 2015",
-			"worklocation" : "Johannesburg - South Africa",
+			"location" : "Greenside, Johannesburg, South Africa",
 			"workdiscription" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In condimentum sit amet risus et elementum. Sed accumsan sem quis orci rhoncus egestas.<br>"
 		},
 		{
 			"position" : "UI designer",
 			"employer" : "Standard Bank Global",
 			"workdates" : "1 July 2015 - Present",
-			"worklocation" : "Johannesburg - South Africa",
+			"location" : "68 Grayston drive, Sandton, South Africa",
 			"workdiscription" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In condimentum sit amet risus et elementum. Sed accumsan sem quis orci rhoncus egestas.<br>"
 		}
 	],
 
-	display : function () {
+	displayWork : function () {
 		for (var cnt in work.jobs) {
 			$("#workExperience").append(HTMLworkStart);
+			$("#workExperience").append(HTMLworkLocation.replace("%data%", work.jobs[cnt].location));
 			$("#workExperience").append(HTMLworkEmployer.replace("%data%", work.jobs[cnt].employer));
 			$("#workExperience").append(HTMLworkTitle.replace("%data%", work.jobs[cnt].position));
 			$("#workExperience").append(HTMLworkDates.replace("%data%", work.jobs[cnt].workdates));
-			$("#workExperience").append(HTMLworkLocation.replace("%data%", work.jobs[cnt].worklocation));
 			$("#workExperience").append(HTMLworkDescription.replace("%data%", work.jobs[cnt].workdiscription));
 		}
 	}
 };
 
-work.display();
+work.displayWork();
 
 /*
-Project
+Brands
 */
 
 var projects =
@@ -80,28 +102,32 @@ var projects =
 	"brands" :
 	[
 		{
+			"brandtitle" : "Standard Bank",
+			"brandrole" : "Lead UI designer - Online banking",
+			"date" : "Current project",
+			"brandDescription" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+			"brandImage" : "images/197x148_4.png"
+		},
+		{
 			"brandtitle" : "South African Tourism",
-			"date" : "2015",
+			"brandrole" : "Digital Advertising - Team lead",
+			"date" : "2014 - 2015",
 			"brandDescription" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-			"brandImage" : "images/197x148.gif"
+			"brandImage" : "images/197x148_1.png"
 		},
 		{
 			"brandtitle" : "Vodacom",
-			"date" : "2014-2015",
+			"brandrole" : "Digital Advertising - Mid-creative",
+			"date" : "2013-2014",
 			"brandDescription" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-			"brandImage" : "images/197x148.gif"
+			"brandImage" : "images/197x148_2.png"
 		},
 		{
 			"brandtitle" : "First National Bank",
+			"brandrole" : "Digital Advertising - Junior-creative",
 			"date" : "2012-2014",
-			"brandDescription" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-			"brandImage" : "images/197x148.gif"
-		},
-		{
-			"brandtitle" : "Standard Bank",
-			"date" : "2015",
-			"brandDescription" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-			"brandImage" : "images/197x148.gif"
+			"brandDescription" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Consectetur adipiscing elit.",
+			"brandImage" : "images/197x148_3.png"
 		}
 	],
 
@@ -110,11 +136,11 @@ var projects =
 		for (var cnt in projects.brands)
 		{
 			$("#projects").append(HTMLprojectStart);
-			$("#projects").append(HTMLprojectTitle.replace("%data%", projects.brands[cnt].brandtitle));
 			$("#projects").append(HTMLprojectImage.replace("%data%", projects.brands[cnt].brandImage));
+			$("#projects").append(HTMLprojectTitle.replace("%data%", projects.brands[cnt].brandtitle));
+			$("#projects").append(HTMLprojectRole.replace("%data%", projects.brands[cnt].brandrole));
 			$("#projects").append(HTMLprojectDates.replace("%data%", projects.brands[cnt].date));
-			$("#projects").append(HTMLprojectDescription.replace("%data%", projects.brands[cnt].brandDescription));
-
+			//$("#projects").append(HTMLprojectDescription.replace("%data%", projects.brands[cnt].brandDescription));
 		}
 	}
 };
@@ -132,10 +158,10 @@ var education =
 	[
 		{
 			"schoolName" : "North-West Univercity of Potchefstroom",
-			"degree" : "BA",
-			"schoolDates" : "2009-2011",
-			"schoolLocation" : "Potchefstroom - South Africa",
-			"major" : "Graphic design - Multi-media",
+			"degree" : "BA Graphic design & Multi-media",
+			"schoolDates" : "2008-2011",
+			"location" : "Potchefstroom, South Africa",
+			//"major" : "Graphic design - Multi-media",
 			"onlineCourse" : "Front-end development",
 			"onlineSchool" : "Udacity Nano-degree",
 			"onlineDates" : "2015-2016",
@@ -145,14 +171,14 @@ var education =
 	],
 	display : function ()
 	{
-		for (var cnt in projects.brands)
+		for (var cnt in education.edu)
 		{
 			$("#education").append(HTMLschoolStart);
+			$("#education").append(HTMLschoolLocation.replace("%data%", education.edu[cnt].location));
 			$("#education").append(HTMLschoolName.replace("%data%", education.edu[cnt].schoolName));
 			$("#education").append(HTMLschoolDegree.replace("%data%", education.edu[cnt].degree));
 			$("#education").append(HTMLschoolDates.replace("%data%", education.edu[cnt].schoolDates));
-			$("#education").append(HTMLschoolLocation.replace("%data%", education.edu[cnt].schoolLocation));
-			$("#education").append(HTMLschoolMajor.replace("%data%", education.edu[cnt].major));
+			//("#education").append(HTMLschoolMajor.replace("%data%", education.edu[cnt].major));
 			$("#education").append(HTMLonlineClasses);
 			$("#education").append(HTMLonlineTitle.replace("%data%", education.edu[cnt].onlineCourse));
 			$("#education").append(HTMLonlineSchool.replace("%data%", education.edu[cnt].onlineSchool));
@@ -165,11 +191,7 @@ var education =
 
 education.display();
 
-
-/*
-
-*/
-
+	$("#mapDiv").append(googleMap);
 
 
 
